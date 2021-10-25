e5f598ac68efc4f9c0d3499e9831085516239376
Adelina <aadelya2702@gmail.com>
2021-10-25 19:05:39 +0600

    25.10.21

---
+++ src/components/Section.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Section = () => {
+    return (
+        <>
+            <img src= "http://cdn.shopify.com/s/files/1/0294/8231/9925/products/qmCjzf3E6m_596a15c6-48da-441a-8d76-617309202cd4_1200x1200.jpg?v=1602586449"></img>
+        </>
+    );
+};
+
+export default Section;
